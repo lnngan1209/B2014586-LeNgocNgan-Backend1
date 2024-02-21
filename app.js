12cd80e5abe3bb477906ca7295800c3bfe6960c0
lnngan1209 <sakamoto12416@gmail.com>
2024-02-21 08:34:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+// const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -8,6 +11,18 @@ app.use(express.json());
 
 app.get("/", (req,res) => {
     res.json({message: "Welcome to contact book application"});
+});
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
