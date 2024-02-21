58a290b2092577a1238b2f72d9adeb6cfff1e852
lnngan1209 <sakamoto12416@gmail.com>
2024-02-21 09:05:03 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
-// const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -19,7 +19,8 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
+    console.error("Error handling middleware triggered:", error);
     return res.status(error.statusCode || 500).json({
         message: error.message || "Internal Server Error",
     });
